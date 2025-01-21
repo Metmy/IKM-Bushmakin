3c1c2760378d0799d9735983a6901c04f006797c
Metmy <78279920+Metmy@users.noreply.github.com>
2025-01-21 10:52:54 +0500

    code.cpp

--- code.cpp
+++ code.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "lcm.h"
 #include <string>
 
 using namespace std;
+
+bool durak(const string& str) {
+    for (char const &c : str) {
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+    return true;
+}
 
 string nod(string a, string b) {
     while (b != "0") {
@@ -19,10 +27,20 @@ string nok(string m, string n) {
 
 int main() {
     string m, n;
+    
     cout << "Введите число m: ";
     cin >> m;
+    if (!durak(m)) {
+        cout << "Ошибка" << endl;
+        return 1;
+    }
+
     cout << "Введите число n: ";
     cin >> n;
+    if (!durak(n)) {
+        cout << "Ошибка" << endl;
+        return 1;
+    }
 
     string r = nok(m, n);
     cout << "Наименьшее общее кратное m и n: " << r << endl;
